a052fe0f4ed5147b79fc9a6d4fffef4d1026197e
Kailash <klskailash642@gmail.com>
2023-08-17 10:56:04 +0545

    docker file config

--- restapi/src/main/java/com/spring/restapi/restapi/dao/UserDao.java
+++ restapi/src/main/java/com/spring/restapi/restapi/dao/UserDao.java
@@ -2,7 +2,6 @@ package com.spring.restapi.restapi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.spring.restapi.restapi.entities.User;
 
 @Repository
--- restapi/src/test/java/com/spring/restapi/restapi/RestapiApplicationTests.java
+++ restapi/src/test/java/com/spring/restapi/restapi/RestapiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.spring.restapi.restapi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestapiApplicationTests {
-
 	@Test
 	void contextLoads() {
+		int a = 10, b = 20;
+		int sum = sumOfTwoNum(a, b);
+
+		assertEquals(30, sum);
+	}
+	public int sumOfTwoNum(int a, int b) {
+		return a + b;
 	}
 
 }
